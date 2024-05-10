dc654c8822c76fba97cc7be31f86a23204e8e722
GL-Li <liguanglai@gmail.com>
2024-05-10 06:36:06 -0400

    add timeout function

---
+++ R/timeout.R
@@ -0,0 +1,25 @@
+#' Stop the execution of an expression if the elapsed time exceed a limit
+#'
+#' @param expr R expression such as sum(1:5) and muli-line R code inside {}.
+#' @param timeout numeric, time limit in second
+#'
+#' @return return of expr
+#'
+#' @examples
+#' timeout(
+#'   {
+#'     for (i in 1:1e8) {
+#'       print(i)
+#'     }
+#'   },
+#'   timeout = 0.1
+#' )
+#'
+#' @export
+#'
+
+timeout <- function(expr, timeout) {
+  setTimeLimit(elapsed = timeout)
+  on.exit(setTimeLimit(elapsed = Inf))
+  expr
+}
